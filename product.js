8ed41a50867167eb795bd7a8db6e366e496020b4
Abir0248 <abir15-6789@diu.edu.bd>
2023-07-04 21:40:42 +0600

    export

--- product.js
+++ product.js
@@ -1,5 +1,7 @@
-export const products = [
-  { id: 1, name: "Product 1", price: 100 },
-  { id: 2, name: "Product 2", price: 200 },
-  { id: 3, name: "Product 3", price: 300 },
+const products = [
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
 ];
+
+export default products;
