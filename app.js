94d0b7a996068860b19485259d337e4bf7328f9d
Abir0248 <abir15-6789@diu.edu.bd>
2023-07-04 21:13:51 +0600

    display cart

--- app.js
+++ app.js
@@ -21,4 +21,27 @@ function displayProducts() {
   });
 }
 
+function displayCart() {
+  const cartDiv = document.getElementById("cart");
+  cartDiv.innerHTML = "";
+  cart.forEach((item) => {
+    const cartItemDiv = document.createElement("div");
+    cartItemDiv.classList.add("cart-item");
+    cartItemDiv.innerHTML = `
+        <h5>${item.product.name}</h5>
+        <p>Quantity: ${item.quantity}</p>
+        <p>Price: $${item.product.price}</p>
+        <p>Total: $${item.product.price * item.quantity}</p>
+        <button class="remove-from-cart btn btn-danger" data-id="${
+          item.product.id
+        }">Remove</button>
+      `;
+    cartDiv.appendChild(cartItemDiv);
+  });
+  const totalDiv = document.createElement("div");
+  totalDiv.innerHTML = `<h4>Total: $${calculateTotal()}</h4>`;
+  cartDiv.appendChild(totalDiv);
+}
+
 displayProducts();
+displayCart();
